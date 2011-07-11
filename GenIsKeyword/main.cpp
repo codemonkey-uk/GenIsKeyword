55da4d1e846ffe8d153a06659e9c34b064dc4cc5
Thaddaeus Frogley <codemonkey_uk@thaddaeus-frogleys-computer.local>
2011-07-11 20:56:55 +0100

    Initial Commit

---
+++ GenIsKeyword/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GenIsKeyword
+//
+//  Created by Thaddaeus Frogley on 11/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
